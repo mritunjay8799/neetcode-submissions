afa31517a13d02af6051fa739f4e0fd01544abf0
Mritunjay Tripathi <36035989+mritunjay8799@users.noreply.github.com>
2026-06-04 10:32:24 +0530

    Add: last-stone-weight - submission-3

---
+++ Data Structures & Algorithms/last-stone-weight/submission-3.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> maxHeap(stones.begin(), stones.end());  // O(n) heapify 
+        /* 
+           O(nlogn) - Pushing one by one
+           priority_queue<int>maxHeap;
+        // int n = stones.size();
+        // for(int i = 0; i < n; i++){
+        //     maxHeap.push(stones[i]);
+         }*/
+        while(maxHeap.size() > 1){
+            int a = maxHeap.top();
+            maxHeap.pop();
+            int b = maxHeap.top();
+            maxHeap.pop();
+            int res = (a-b);
+            if(res > 0)
+                maxHeap.push(res);
+        }
+        if(maxHeap.size() > 0)
+            return maxHeap.top();
+        return 0;
+    }
+};
